f859bf7e7a6f8b6f8e9ef874ca9cf43febcfce93
Kapil Kumar <kapilkumar0037@gmail.com>
2025-12-08 22:47:40 +0530

    prep

--- src/app/components/articles/articles.component.ts
+++ src/app/components/articles/articles.component.ts
@@ -1,12 +1,11 @@
-import { provideHttpClient } from '@angular/common/http';
+
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-articles',
   imports: [],
   templateUrl: './articles.component.html',
-  styleUrl: './articles.component.scss',
-  providers: [provideHttpClient()]
+  styleUrl: './articles.component.scss'
 })
 export class ArticlesComponent {
 
